79b3274e429239972a9d3a6a33c346127c3aeeb0
Mészáros Árpád <meszaros.arpad@outlook.hu>
2022-01-31 18:25:53 +0100

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static long fibonacci(int n)
+        {
+            if (n < 2) return n;
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Mennyi szám jelenjen meg? ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < szam; i++)
+            {
+                Console.WriteLine(fibonacci(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
